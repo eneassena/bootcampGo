8102ff1d1cdc7f805e85c3d3da2fc09799bf922a
eneassena10 <eneas.sena@mercadolivre.com>
2022-07-19 10:08:10 -0300

    links para consultas futuras

--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
 module github.com/eneassena/bootcampGo
 
 go 1.13
-
-require (
-	github.com/gin-gonic/gin v1.7.7 // indirect
-	github.com/go-playground/validator/v10 v10.11.0 // indirect
-	github.com/golang/protobuf v1.5.2
-)
--- main.go
+++ main.go
@@ -1,30 +1,5 @@
 package main
-
-import (
-	"fmt"
-
-	"github.com/go-playground/validator/v10"
-)
-
-
-
-type Alunos struct {
-	Name string `json:"name" validate:"required"`
-	Email string `json:"email" validate:"required,uppercase"`
-	Idade string `json:"idade" validate:"required,number"`
- } 
  
-// use a single instance of Validate, it caches struct info
-var validate *validator.Validate
-
-func main() { 
-	a := Alunos{Name: "Carlos",Email: "carlos@hotmail.com", Idade: "14"}	
-	validate = validator.New()
-	err := validate.Struct(a)
-	if err != nil {
-		if _, ok := err.(*validator.ValidationErrors); ok {
-			fmt.Println(err)
-		}
-		fmt.Println(err)
-	}
+func main() {
+	 
 }
